690752d2fae12eca61264f6212053196b6196868
bengths <bengths@student.chalmer.se>
2018-01-14 18:14:01 +0100

    Flappy Fredrik wo sound

---
+++ Project8_v2/Assets/Scripts/FlappyFredrik/GameManagerFF.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerFF : MonoBehaviour {
+
+	public delegate void GameDelegate ();
+	public static event GameDelegate OnGameStarted;
+	public static event GameDelegate OnGameOverConfirmed;
+
+	public static GameManagerFF Instance;
+
+	public GameObject startPage;
+	public GameObject gameOverPage;
+	public GameObject countdownPage;
+	public Text scoreText;
+
+	enum PageState{
+		None,
+		Start,
+		GameOver,
+		CountDown
+	}
+
+	int score = 0;
+	bool gameOver = true;
+
+	public bool GameOver {get { return gameOver; }}
+	public int Score {get { return score; }}
+
+	void Awake() {
+		Instance = this;
+	}
+
+	void OnEnable() {
+		CountDownText.OnCountdownFinished += OnCountdownFinished;
+		PlayerController.OnPlayerDied += OnPlayerDied;
+		PlayerController.OnPlayerScored += OnPlayerScored;
+	}
+		
+
+	void OnDisable() {
+		CountDownText.OnCountdownFinished -= OnCountdownFinished;
+		PlayerController.OnPlayerDied -= OnPlayerDied;
+		PlayerController.OnPlayerScored -= OnPlayerScored;
+	}
+
+	void OnCountdownFinished() {
+		SetPageState (PageState.None);
+		OnGameStarted (); // event sent to PlayerController
+		score = 0;
+		gameOver = false;
+	}
+
+	void OnPlayerDied() {
+		gameOver = true;
+		int savedScore = PlayerPrefs.GetInt ("HighScore");
+		if (score > savedScore) {
+			PlayerPrefs.SetInt ("HighScore", score);
+		}
+		SetPageState (PageState.GameOver);
+	}
+
+	void OnPlayerScored() {
+		score++;
+		scoreText.text = score.ToString();
+	}
+
+
+
+	void SetPageState(PageState state) {
+		switch (state) {
+			case PageState.None:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(false);	
+				break;
+			case PageState.Start:
+				startPage.SetActive(true);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(false);
+				break;
+			case PageState.GameOver:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(true);
+				countdownPage.SetActive(false);
+				break;
+			case PageState.CountDown:
+				startPage.SetActive(false);
+				gameOverPage.SetActive(false);
+				countdownPage.SetActive(true);
+				break;
+		}
+	}
+
+	public void ConfirmGameOver() {
+		// activated when replay button is hit
+		OnGameOverConfirmed(); // event sent to PlayerController
+		scoreText.text = "0";
+		SetPageState (PageState.Start);
+	}
+
+	public void StartGame() {
+		// activated when play button is hit
+		SetPageState(PageState.CountDown);
+	}
+}
---
+++ Project8_v2/Assets/Scripts/FlappyFredrik/PlayerController.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+
+	public delegate void PlayerDelegate ();
+	public static event PlayerDelegate OnPlayerDied;
+	public static event PlayerDelegate OnPlayerScored;
+
+	public float tapForce = 10;
+	public float tiltSmooth = 5;
+	public Vector3 startPos;
+
+	Rigidbody2D rigidbody;
+	Quaternion downRotation;
+	Quaternion forwardRotation;
+
+	GameManagerFF game;
+
+	void Start() {
+		rigidbody = GetComponent<Rigidbody2D> ();
+		downRotation = Quaternion.Euler (0, 0, -90);
+		forwardRotation = Quaternion.Euler (0, 0, 35);
+		game = GameManagerFF.Instance;
+		rigidbody.simulated = false;
+	}
+
+	void OnEnable() {
+		GameManagerFF.OnGameStarted += OnGameStarted;
+		GameManagerFF.OnGameOverConfirmed += OnGameOverConfirmed;
+	}
+
+	void OnDisable() {
+		GameManagerFF.OnGameStarted -= OnGameStarted;
+		GameManagerFF.OnGameOverConfirmed -= OnGameOverConfirmed;
+	}
+
+	void OnGameStarted () { // Reset physics
+		rigidbody.velocity = Vector3.zero;
+		rigidbody.simulated = true;
+
+	}
+
+	void OnGameOverConfirmed ()	{ // Reset position and rotation
+		transform.localPosition = startPos;
+		transform.rotation = Quaternion.identity;
+	}
+
+
+
+
+	void Update() {
+		if (game.GameOver) {
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			transform.rotation = forwardRotation;
+			rigidbody.velocity = Vector3.zero;
+			rigidbody.AddForce (Vector2.up * tapForce, ForceMode2D.Force); 
+		}
+
+		transform.rotation = Quaternion.Lerp (transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D col) {
+		if (col.gameObject.tag == "ScoreZone") {
+			// Register a score Event
+			OnPlayerScored(); // Event sent to GameManagerFF;
+			// Play a sound
+		}
+
+		if (col.gameObject.tag == "DeadZone") {
+			rigidbody.simulated = false;
+			// register a dead event
+			OnPlayerDied(); // Event sent to GameManagerFF;
+			// Play a sound
+		}
+	}
+}
