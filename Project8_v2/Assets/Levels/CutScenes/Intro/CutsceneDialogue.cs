f9ff82c4c755a337b22bea6f7003554b75018cb4
bengths <bengths@student.chalmer.se>
2018-01-26 00:12:52 +0100

    Finished Automatic dialogue boxes for the cutscene

--- Project8_v2/Assets/Levels/CutScenes/Intro/CutsceneDialogue.cs
+++ Project8_v2/Assets/Levels/CutScenes/Intro/CutsceneDialogue.cs
@@ -16,17 +16,25 @@ public class CutsceneDialogue : MonoBehaviour {
 
 	public int currentLine;
 	public int endAtLine;
+	public float typeSpeed;
+
+	private bool isTyping;
+	private bool doneTyping;
+	private bool canSwitchPanel;
 
 	// Use this for initialization
 	void Start () {
 		if (textFile != null) {
 			textLines = (textFile.text.Split ('\n'));
 			displayTime = new float[textLines.Length];
-			// Initialize displayTime
-			for (int i = 0; i < textLines.Length; i++) {
-				string[] tmpStr = (textLines [i].Split ('\t'));
+			isTextboxEnabled = new int[textLines.Length];
 
-				displayTime [i] = float.Parse (tmpStr [0]);
+			// Initialize displayTime
+			for (int i = 0; i < textLines.Length-1; i++) {
+				string[] tmpStr = (textLines [i].Split ('\t'));
+				Debug.Log (tmpStr[0]);
+
+				displayTime [i] = float.Parse(tmpStr [0]);
 				isTextboxEnabled [i] = int.Parse (tmpStr [1]);
 				textLines [i] = tmpStr [2];
 			}
@@ -39,32 +47,62 @@ public class CutsceneDialogue : MonoBehaviour {
 
 		// Make textbox invisible at start
 		DisableTextBox ();
+		isTyping = false;
+		doneTyping = false;
+		canSwitchPanel = true;
+
 	}
 
 	void Update()
 	{
+		if (canSwitchPanel) {
+			canSwitchPanel = false;
 
-		theText.text = textLines [currentLine];
+			if (currentLine > endAtLine) {
+				DisableTextBox ();
+				return;
+			}
 
-		// Increment after displayTime seconds
-		if (Input.GetKeyDown (KeyCode.S)) {
-			currentLine++;
+			if (isTextboxEnabled [currentLine] == 0) {
+				DisableTextBox ();
+				StartCoroutine (IncrementLines ());
+			}
+			if (isTextboxEnabled [currentLine] == 1) {
+				if (!isTyping && !doneTyping) {
+					EnableTextBox ();
+				}
+				StartCoroutine (IncrementLines ());
+
+			}
 		}
-
-		if (isTextboxEnabled) {
-			DisableTextBox ();
-		}
-		if (textLines [currentLine].Equals ("<ENABLE>")) {
-			EnableTextBox ();
-		}
-			
-
 
 
 	}
 
+	private IEnumerator IncrementLines () {
+		yield return new WaitForSeconds (displayTime [currentLine]);
+		currentLine++;
+		doneTyping = false;
+		canSwitchPanel = true;
+	}
+
+	private IEnumerator AutoType (string lineOfText) {
+		theText.text = "";
+		isTyping = true;
+		for(int letter = 0; letter < lineOfText.Length; letter++) {
+			theText.text += lineOfText [letter];
+			yield return new WaitForSeconds (typeSpeed);
+		}
+		theText.text = lineOfText;
+		isTyping = false;
+		doneTyping = true;
+	}
+
+
 	public void EnableTextBox() {
 		textBox.SetActive (true);
+		theText.text = textLines [currentLine];
+		StartCoroutine (AutoType(textLines[currentLine]));
 	}
 
 	public void DisableTextBox() {
